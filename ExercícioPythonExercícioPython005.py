c9e701b62bbcf31da01137f3e31b8449bc99a708
Aristocrata <75492037+Aristocrata@users.noreply.github.com>
2022-04-14 11:34:27 -0300

    Create ExercícioPythonExercícioPython005.py

---
+++ ExercícioPythonExercícioPython005.py
@@ -0,0 +1,8 @@
+# CURSO-EM-VIDEO-PYTHON
+Exercício Python #005
+nome = input('Digite seu nome: '
+n = int(input('Digite um número: '))
+a = n-1
+s = n+1
+print('Analisando o valor, {} Seu antecessor é {}, e seu sucessor é {}! '.format(n, a, s))
+print('Obrigado, {}'.format(nome))
